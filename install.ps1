9658b73bef45fac1c829d47b7f90ea9ca6132e83
Spencer Pollock <spencer.pollock@standardfusion.com>
2024-07-19 10:40:14 -0700

    feat: adding a default profile; added custom theme

--- install.ps1
+++ install.ps1
@@ -1,7 +1,4 @@
 winget install JanDeDobbeleer.OhMyPosh
+
 Install-Module -Name Terminal-Icons -Repository PSGallery
-
-new-item -type file -path $profile -force
-
-Add-Content -Path $profile -Value 'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\iterm2.omp.json" | Invoke-Expression'
-Add-Content -Path $profile -Value "Import-Module -Name Terminal-Icons"
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
